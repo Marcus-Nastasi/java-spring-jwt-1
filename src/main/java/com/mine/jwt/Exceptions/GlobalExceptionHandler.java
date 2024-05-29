5177111a64d61e2d1aa63e05cb253372c152b7f1
rolemberg <vinnie.nsts@gmail.com>
2024-05-29 19:48:29 -0300

    creating token service;

--- src/main/java/com/mine/jwt/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/mine/jwt/Exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.mine.jwt.Exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<String> handleUnexpectedException(Throwable throwable) {
-        return ResponseEntity.badRequest().body("{\"error\": " + "\"" + throwable.getMessage() + "\"" + '}');
+        String errorMessage = "{\"error\": " + "\"" + throwable.getMessage() + "\"" + '}';
+        logger.error(errorMessage, throwable);
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 }
 
---
+++ src/main/java/com/mine/jwt/Services/TokenService.java
@@ -0,0 +1,56 @@
+package com.mine.jwt.Services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.mine.jwt.Models.Domain.Users.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+    private final Algorithm algorithm = Algorithm.HMAC256(this.secret);
+
+    public String generate(User user) {
+        try {
+
+            return(
+                JWT.create()
+                .withIssuer("auth-api")
+                .withSubject(user.getEmail()).withExpiresAt(genExpDate())
+                .sign(algorithm)
+            );
+        } catch (JWTCreationException e) {
+            return "";
+        }
+    }
+
+    public String validate(String token) {
+        try {
+            return(JWT
+                .require(this.algorithm)
+                .withIssuer("auth-api")
+                .build()
+                .verify(token)
+                .getSubject()
+            );
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private Instant genExpDate() {
+        return LocalDateTime.now().plusMinutes(15).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
+
+
+
